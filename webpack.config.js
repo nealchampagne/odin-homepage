c338d125ccfbc5022b575a8b6c3c3825bbfbdeb6
Neal Champagne <166440916+nealchampagne@users.noreply.github.com>
2024-11-07 08:06:31 -0800

    Initialize files for homepage project

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      title: "Odin Homepage",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  module: {
+    rules: [
+      // {
+      //   test: /\.css$/i,
+      //   use: ["style-loader", "css-loader"],
+      // },
+      {
+        test: /\.(png|svg|jpe?g|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    hot: true,
+    watchFiles: ["./src/*"],
+    static: {
+      directory: path.join(__dirname, "dist"),
+      watch: true,
+    },
+  },
+};
